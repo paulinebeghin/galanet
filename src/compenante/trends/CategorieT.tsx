76b56288590f412f995c062b9e2e12156c03ace6
pauline beghin <paulinebeghin7@gmail.com>
2026-01-07 13:20:49 +0100

    :boom: finsh

--- src/compenante/trends/CategorieT.tsx
+++ src/compenante/trends/CategorieT.tsx
@@ -1,16 +1,57 @@
-interface Props {
-    bgImage: string;
-    texte: string;
-    herf: string;
-    
-}
+import type { ReactNode } from "react"
 
-const CategorieT = () => {
+interface Props {
+    bgImage?: string;
+    texte?: string;
+    href?: string;
+    showIcon?: boolean;
+    icon?: ReactNode;
+}
+//  w-[268.8px]
+const CategorieT = ({bgImage,texte,href,showIcon,icon}:Props) => {
     return (
-        <div className="bg-[url('/vaisseaux_de_fuite.png')]">
-            
-        </div>
+        <>
+        {
+            showIcon?
+         <a
+            href={href}
+            className="flex justify-start items-end p-4 bg-cover bg-center h-full rounded-lg w-1/5"
+
+            style={{ backgroundImage: `url(${bgImage})` }}
+        >
+            <h5 className="font-bold text-lg text-white">
+                {texte}
+            </h5>
+        </a>
+        :
+        <a
+           href={href}
+           className=" p-6 bg-cover bg-center h-full w-1/5 rounded-lg  bg-violet-600 flex flex-col gap-5"
+
+           
+       >
+           <h5 className="font-bold text-2xl text-white">
+               {icon}{texte} 
+           </h5>
+       </a>
+        }
+        </>
     )
 }
+
+// const CategorieT = ({bgImage,texte,href}:Props) => {
+//     return (
+//          <a
+//             href={href}
+//             className="flex justify-start items-end p-4 bg-cover bg-center h-full w-[268.8px] rounded-lg"
+
+//             style={{ backgroundImage: `url(${bgImage})` }}
+//         >
+//             <h5 className="font-bold text-lg text-white">
+//                 {texte}
+//             </h5>
+//         </a>
+//     )
+// }
 
 export default CategorieT
--- src/compenante/trends/Trends.tsx
+++ src/compenante/trends/Trends.tsx
@@ -1,6 +1,15 @@
+import CategorieT from "./CategorieT"
+import { Flame } from 'lucide-react';
 const Trends = () => {
     return (
-        <div className="gap-6">
+        <div className="gap-6 h-80 flex">
+            
+            <CategorieT href="" texte="Cargaisons chaudes" showIcon={false} icon={<Flame/>}/>
+            <CategorieT href="" bgImage="vaisseaux_de_fuite.png" texte="Vaisseaux de Fuite"showIcon={true}/>
+            <CategorieT href="" bgImage="arsenal_clandesdin.png" texte="Arsenal Clandestin" showIcon={true}/>
+            <CategorieT href="" bgImage="composants_ia.png" texte="Composants & IA" showIcon={true}/>
+            <CategorieT href="" bgImage="reliques_perdues.png" texte="Reliques Perdues" showIcon={true}/>
+
             
         </div>
     )
